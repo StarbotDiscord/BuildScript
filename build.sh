cb3907570ad95f17a613dbdf311904efb8132854
Sydney Erickson <sydneyerickson@icloud.com>
2017-03-14 12:39:57 -0700

    Changing download system to CURL and readline URL

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 set -e
 
-wget ftp://ftp.cwru.edu/pub/bash/readline-7.0.tar.gz
-tar xf readline-7.0.tar.gz
+curl https://ftp.gnu.org/gnu/readline/readline-7.0.tar.gz > readline.tar.gz
+tar xf readline.tar.gz
 mv readline-7.0 readline
 cd readline
 ./configure --prefix=$HOME/Starbot.framework
