bc951fb980a8656688ad3f9209a3fcae138c77e2
Sydney Erickson <sydneyerickson@icloud.com>
2017-03-14 16:56:36 -0700

    Fixing typo and slight mods to build order

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ rm -rf lbzip2*
 rm -rf gperf*
 rm -rf libiconv*
 rm -rf expat*
-rm -rf gettest*
+rm -rf gettext*
 rm -rf ncurses*
 
 export INSTALLDIR=$HOME/Starbot.framework
@@ -31,12 +31,13 @@ function downloadAndCompile {
     ./configure --prefix=$INSTALLDIR $3
     make
     make install
+    cd ..
 }
 
 downloadAndCompile lbzip2-2.5 http://archive.lbzip2.org/lbzip2-2.5.tar.gz
 downloadAndCompile gperf-3.1 http://ftp.gnu.org/pub/gnu/gperf/gperf-3.1.tar.gz
 downloadAndCompile libiconv-1.15 https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.15.tar.gz "--with-gperf=$INSTALLDIR"
 downloadAndCompile expat-2.2.0 https://sydneyerickson.me/mirror/expat-2.2.0.tar.bz2 "--with-lbzip2=$INSTALLDIR"
-downloadAndCompile gettest-0.19.8.1 https://ftp.gnu.org/pub/gnu/gettext/gettext-0.19.8.1.tar.gz "--with-expat=$INSTALLDIR --with-libiconv=$INSTALLDIR --with-ncurses=$INSTALLDIR"
 downloadAndCompile ncurses-6.0 https://ftp.gnu.org/gnu/ncurses/ncurses-6.0.tar.gz
-downloadAndCompile xz-5.3.2 http://tukaani.org/xz/xz-5.2.3.tar.gz
+downloadAndCompile gettext-0.19.8.1 https://ftp.gnu.org/pub/gnu/gettext/gettext-0.19.8.1.tar.gz "--with-expat=$INSTALLDIR --with-libiconv=$INSTALLDIR --with-ncurses=$INSTALLDIR"
+downloadAndCompile xz-5.3.2 http://tukaani.org/xz/xz-5.2.3.tar.gz "--with-lbzip2=$INSTALLDIR --with-libiconv=$INSTALLDIR --with-gettext=$INSTALLDIR"
