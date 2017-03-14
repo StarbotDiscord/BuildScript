3b09a9d51c471c517e1fb0dc768b75852997ed60
Sydney Erickson <sydneyerickson@icloud.com>
2017-03-14 16:46:36 -0700

    Hopefully working build script to build the XZ part of dep tree

--- build.sh
+++ build.sh
@@ -1,9 +1,42 @@
+# Python 3.5
+# |_ XZ
+#    |_ lbzip2
+#    |_ libiconv
+#    |  |_ gperf
+#    |_ gettext
+#       |_ expat
+#       |  |_ lbzip2 (installed before)
+#       |_ libiconv (installed before)
+#       |_ ncurses
+
+
+rm -rf lbzip2*
+rm -rf gperf*
+rm -rf libiconv*
+rm -rf expat*
+rm -rf gettest*
+rm -rf ncurses*
+
+export INSTALLDIR=$HOME/Starbot.framework
+
 set -e
 
-curl https://ftp.gnu.org/gnu/readline/readline-7.0.tar.gz > readline.tar.gz
-tar xf readline.tar.gz
-mv readline-7.0 readline
-cd readline
-./configure --prefix=$HOME/Starbot.framework
-make
-make install
+# $1 - Project name
+# $2 - Project URL
+# $3 - Additional config args
+function downloadAndCompile {
+    curl $2 > $1.tar.gz
+    tar xf $1.tar.gz
+    cd $1
+    ./configure --prefix=$INSTALLDIR $3
+    make
+    make install
+}
+
+downloadAndCompile lbzip2-2.5 http://archive.lbzip2.org/lbzip2-2.5.tar.gz
+downloadAndCompile gperf-3.1 http://ftp.gnu.org/pub/gnu/gperf/gperf-3.1.tar.gz
+downloadAndCompile libiconv-1.15 https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.15.tar.gz "--with-gperf=$INSTALLDIR"
+downloadAndCompile expat-2.2.0 https://sydneyerickson.me/mirror/expat-2.2.0.tar.bz2 "--with-lbzip2=$INSTALLDIR"
+downloadAndCompile gettest-0.19.8.1 https://ftp.gnu.org/pub/gnu/gettext/gettext-0.19.8.1.tar.gz "--with-expat=$INSTALLDIR --with-libiconv=$INSTALLDIR --with-ncurses=$INSTALLDIR"
+downloadAndCompile ncurses-6.0 https://ftp.gnu.org/gnu/ncurses/ncurses-6.0.tar.gz
+downloadAndCompile xz-5.3.2 http://tukaani.org/xz/xz-5.2.3.tar.gz
