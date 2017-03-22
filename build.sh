1cd5fb13fd81d05975175293743aec308befac04
Sydney Erickson <sydneyerickson@icloud.com>
2017-03-22 02:24:59 -0700

    Update SSL certs for macOS version of Starbot Runtime

--- build.sh
+++ build.sh
@@ -136,8 +136,13 @@ cd ..
 downloadAndCompile libedit-20160903-3.1 http://thrysoee.dk/editline/libedit-20160903-3.1.tar.gz "--with-ncurses=$INSTALLDIR"
 downloadAndCompile sqlite-autoconf-3170000 http://www.sqlite.org/2017/sqlite-autoconf-3170000.tar.gz "--with-libedit=$INSTALLDIR --with-ncurses=$INSTALLDIR"
 cp -r $INSTALLDIR/lib $HOME/lib
-downloadAndCompile Python-3.5.3 https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
+downloadAndCompile Python-3.5.3 https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
 rm -rf $HOME/lib
+
+if [[ "$unamestr" == 'Darwin' ]]; then
+    ./updatessl.sh
+fi
+
 $INSTALLDIR/bin/pip3 install discord.py requests pluginbase psutil gitpython pyparsing
 
 #  _      _ _      ____  _____  _    _  _____ 
