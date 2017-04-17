f8898a97ae7022f9db3aa4965169abe090fce02a
Sydney Erickson <sydneyerickson@icloud.com>
2017-04-17 15:27:14 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -95,7 +95,7 @@ set -e
 # $3 - Additional config args
 function downloadAndCompile {
     curl $2 > $1.tar.gz
-    tar -xzf $1.tar.gz
+    tar -xf $1.tar.gz
     cd $1
     ./configure --prefix=$INSTALLDIR $3
     make $MAKEARGS
