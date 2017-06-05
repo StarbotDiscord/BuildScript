aad9d7329faf126444f87ba34c18e9378e186d9a
Sydney Erickson <sydneyerickson@icloud.com>
2017-06-04 23:05:23 -0700

    Fix pkg-config version number

--- build.sh
+++ build.sh
@@ -167,5 +167,5 @@ $INSTALLDIR/bin/pip3 install discord.py pluginbase psutil pyparsing pyspeedtest 
 # | |____| | |_) | |__| | |    | |__| |____) |
 # |______|_|_.__/ \____/|_|     \____/|_____/ 
 
-downloadAndCompile pkg-config-0.29.1 https://pkg-config.freedesktop.org/releases/pkg-config-0.29.2.tar.gz "--with-iconv=$INSTALLDIR --with-internal-glib"
+downloadAndCompile pkg-config-0.29.2 https://pkg-config.freedesktop.org/releases/pkg-config-0.29.2.tar.gz "--with-iconv=$INSTALLDIR --with-internal-glib"
 downloadAndCompile opus-1.1.4 http://downloads.xiph.org/releases/opus/opus-1.1.4.tar.gz
